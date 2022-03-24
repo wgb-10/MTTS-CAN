7429e3ab7eb1aabdaef457e3e9f34cfb3d1dcab4
Wesley Barreto <wesley.barreto610@gmail.com>
2022-03-24 14:17:57 +0400

    add more comments

--- code/pre_process.py
+++ code/pre_process.py
@@ -6,20 +6,15 @@ import numpy as np
 import scipy.io
 
 
-def get_nframe_video(path):
+# Input: Path to video. Returns: Number of frames in video
+# TODO: Modify function to match above specification
+def get_nframe_of_video(path):
     temp_f1 = h5py.File(path, 'r')
     temp_dysub = np.array(temp_f1["dysub"])
     nframe_per_video = temp_dysub.shape[0]          # In this case no. frames = no. of GT (ground truth) measurements
     return nframe_per_video
 
-
-def get_nframe_video_val(path):
-    temp_f1 = scipy.io.loadmat(path)
-    temp_dXsub = np.array(temp_f1["dXsub"])
-    nframe_per_video = temp_dXsub.shape[0]
-    return nframe_per_video
-
-# TODO: Find out how this works
+# TODO: Modify this function to split subjects in another way
 def split_subj(data_dir, cv_split, subNum):
     f3 = h5py.File(data_dir + '/M.mat', 'r')
     M = np.transpose(np.array(f3["M"])).astype(np.bool)
@@ -29,7 +24,6 @@ def split_subj(data_dir, cv_split, subNum):
 
 
 # TODO: Test if sort_video_list works 
-
 def take_last_ele(ele):
     ele = ele.split('.')[0][-2:]
     try:
@@ -38,24 +32,12 @@ def take_last_ele(ele):
         return int(ele[-1:])
 
 
-def sort_video_list(data_dir, sub_list):
+def sort_video_list(data_dir, taskList, subTrain):
     final = []
-    for sub in sub_list:
-        x = glob.glob(os.path.join(data_dir, "subject" + str(sub) + "/vid.avi"))
-        x = sorted(x)
-        x = sorted(x, key=take_last_ele)
-        final.append(x)
+    for p in subTrain:
+        for t in taskList:
+            x = glob.glob(os.path.join(data_dir, 'P' + str(p) + 'T' + str(t) + 'VideoB2*.mat'))
+            x = sorted(x)
+            x = sorted(x, key=take_last_ele)
+            final.append(x)
     return final
-
-
-# Old code
-
-# def sort_video_list(data_dir, taskList, subTrain):
-#     final = []
-#     for p in subTrain:
-#         for t in taskList:
-#             x = glob.glob(os.path.join(data_dir, 'P' + str(p) + 'T' + str(t) + 'VideoB2*.mat'))
-#             x = sorted(x)
-#             x = sorted(x, key=take_last_ele)
-#             final.append(x)
-#     return final
